11943522e073e7e31bd91aa534ee5a609b6d0753
gowtham <gowthamm.5127@gmail.com>
2022-05-21 00:50:53 +0530

    update binary search description

--- datastructure/Binary_Search.js
+++ datastructure/Binary_Search.js
@@ -1,8 +1,16 @@
 /**
  * Binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array
  * Binary search compares the target value to the middle element of the array. 
- * If they are not equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half, again taking the middle element to compare to the target value, and repeating this until the target value is found.
- * If the search ends with the remaining half being empty, the target is not in the array.
+    If they are not equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half, 
+    again taking the middle element to compare to the target value, and repeating this until the target value is found. 
+    If the search ends with the remaining half being empty, the target is not in the array. 
+*/
+
+ /** 
+  * Begin with an interval covering the whole array.
+  *  If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
+  * Otherwise, narrow it to the upper half.
+  * Repeatedly check until the value is found or the interval is empty.
  */
 
  let arr = [5,8,16,23,43,56,76,123,456,765];
