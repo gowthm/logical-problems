fa366437f8169de6de26d0eaf008c4887e4ecf50
Gowtham <gowthamm.5127@gmail.com>
2023-05-30 02:45:50 +0530

    Update zig_zag_conversion.js

--- leet_code/zig_zag_conversion.js
+++ leet_code/zig_zag_conversion.js
@@ -1,12 +1,22 @@
 let s = "PAYPALISHIRING";
 // Output: "PAHNAPLSIIGYIR"
+
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Input: s = "A", numRows = 1
+// Output: "A"
+
 let numRows = 3;
 console.log(zigzagConversion(s, numRows))
 function zigzagConversion(s, numRows) {
     let rev = false;
     let curRow = 0;
     let row = [];
-    let result = ''
+    let result = '';
+    if (numRows == 1) {
+        return s
+    }
     for (let i=0; i<numRows; i++) {
         row[i] = [];
     }
