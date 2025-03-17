365399e87ef16fafdf01479d7fc93de1364b986c
gowtham <gowthamm.5127@gmail.com>
2025-03-18 00:32:30 +0530

    Added score of a string

---
+++ leet_code/score_of_a_string.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    s = s.split("");
+    let t = 0;
+    s.map((e,l) => {
+        if (l != s.length-1) {
+            // console.log(e,l, s.length-1);
+            console.log(s[l].charCodeAt(0), s[l+1].charCodeAt(0))
+            t += Math.abs(s[l].charCodeAt(0)-s[l+1].charCodeAt(0));
+        }
+        
+    })
+    return t
+    
+};
+
+console.log(scoreOfString("hello"))
+
+
+// Example 1:
+
+// Input: s = "hello"
+
+// Output: 13
+
+// Explanation:
+
+// The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+// Example 2:
+
+// Input: s = "zaz"
+
+// Output: 50
+
+// Explanation:
+
+// The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+ 
+
+// Constraints:
+
+// 2 <= s.length <= 100
+// s consists only of lowercase English letters.
