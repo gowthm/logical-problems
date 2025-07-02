05e48afdae2bce4dd850fa81c487f3b6b32437f0
gowtham <gowthamm.5127@gmail.com>
2025-07-03 02:54:17 +0530

    Code logic fix in find_index_first_occurrence_string

--- leet_code/find_index_first_occurrence_string.js
+++ leet_code/find_index_first_occurrence_string.js
@@ -4,22 +4,14 @@ let needle = "sad";
 // let haystack = "leetcode";
 // let needle = "leeto";
 
-let listIndex = []
-
-
-var strStr = function(haystack, needle) {
-
-let listIndex = []
-for (let i=0; i<haystack.length; i++) {
-    if (haystack[i] == needle[0]) {
-        let tempNeedle = haystack.slice(i, i+needle.length);
-        if (needle == tempNeedle) {
-            listIndex.push(i);
-        }
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    let tempNeedle = haystack.slice(i, i + needle.length);
+    if (needle == tempNeedle) {
+      return i;
     }
-}
-return listIndex.length == 0 ? -1 : listIndex[0];
-    
+  }
+  return -1;
 };
 
-console.log(strStr(haystack,needle))
+console.log(strStr(haystack, needle));
