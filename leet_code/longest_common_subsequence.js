a30d5e23d9a3bfe0a4fbd1a52a42a9e00319ce58
gowtham <gowthamm.5127@gmail.com>
2025-10-27 01:36:12 +0530

    Added longest common subsequence

---
+++ leet_code/longest_common_subsequence.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+    let dp = Array.from({ length: text1.length + 1 }, () => Array(text2.length + 1).fill(0));
+
+    for (let i = 1; i <= text1.length; i++) {
+        for (let j = 1; j <= text2.length; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[text1.length][text2.length];
+};
+
+
+let text1 = "abcde", text2 = "acr";
+console.log(longestCommonSubsequence(text1, text2))
+
+// Example 1:
+
+// Input: text1 = "abcde", text2 = "ace"
+// Output: 3
+// Explanation: The longest common subsequence is "ace" and its length is 3.
+// Example 2:
+
+// Input: text1 = "abc", text2 = "abc"
+// Output: 3
+// Explanation: The longest common subsequence is "abc" and its length is 3.
+// Example 3:
+
+// Input: text1 = "abc", text2 = "def"
+// Output: 0
+// Explanation: There is no such common subsequence, so the result is 0.
