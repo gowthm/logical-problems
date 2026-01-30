efc15e7b27134797873740d2db625d43a5ff9215
gowtham <gowthamm.5127@gmail.com>
2026-01-30 23:45:40 +0530

    Added majority element I

--- leet_code/longest_common_prefix.js
+++ leet_code/longest_common_prefix.js
@@ -1,16 +1,16 @@
 // let str = ["flower","flow","flight"];
-let str = ["dog","racecar","car"]
+let str = ["dog", "racecar", "car"]
 
-let longestCommonPrefix = function(strs) {
+let longestCommonPrefix = function (strs) {
     if (!strs.length) {
         return 'zx';
-        
+
     }
-    let max_length_str = strs.sort((m,n) => {return m.length - n.length});
-    let max_str = max_length_str[max_length_str.length-1];
-    for (let m = 0; m<max_str.length; m++) {
+    let max_length_str = strs.sort((m, n) => { return m.length - n.length });
+    let max_str = max_length_str[max_length_str.length - 1];
+    for (let m = 0; m < max_str.length; m++) {
         for (let g of strs) {
-            
+
             if (g[m] != max_str[m]) {
                 return g.slice(0, m).length > 0 ? g.slice(0, m) : ''
             }
