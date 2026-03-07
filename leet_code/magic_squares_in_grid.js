5cfac3603406078453c0d18dd5c0cf2f91dff2d0
gowtham <gowthamm.5127@gmail.com>
2026-03-07 11:08:16 +0530

    Added magic squares in grid

--- leet_code/magic_squares_in_grid.js
+++ leet_code/magic_squares_in_grid.js
@@ -62,6 +62,7 @@ var numMagicSquaresInside = function (grid) {
 let grid = [[10, 3, 5], [1, 6, 11], [7, 9, 2]]
 console.log(numMagicSquaresInside(grid));
 
+
 // Input: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]]
 // Output: 1
 
