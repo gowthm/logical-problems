a38d014490032cdefab5948ac31065e383cca25b
gowtham <gowthamm.5127@gmail.com>
2026-03-08 23:04:36 +0530

    Initiate Statisfiability of equality equations

--- leet_code/statisfiability_of_equality_equations.js
+++ leet_code/statisfiability_of_equality_equations.js
@@ -2,10 +2,47 @@
  * @param {string[]} equations
  * @return {boolean}
  */
-var equationsPossible = function(equations) {
-    
+var equationsPossible = function (equations) {
+
+    let parent = new Array(26).fill(0).map((_, i) => i);
+
+    function find(x) {
+        if (parent[x] !== x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x];
+    }
+
+    function union(x, y) {
+        parent[find(x)] = find(y)
+
+    }
+
+    // Step 1 handle '=' 
+
+    for (let equation of equations) {
+        if (equation[1] === '=') {
+            let a = equation.charCodeAt(0) - 97;
+            let b = equation.charCodeAt(3) - 97;
+            union(a, b)
+        }
+    }
+
+    // Step 2 handle '!'
+    for (let equation of equations) {
+        if (equation[1] === '!') {
+            let a = equation.charCodeAt(0) - 97;
+            let b = equation.charCodeAt(3) - 97;
+            if (find(a) == find(b)) {
+                return false;
+            }
+        }
+    }
+    return true;
 };
 
+let equations = ["a==b", "b!=a"];
+console.log(equationsPossible(equations));
 
 
 // Example 1:
