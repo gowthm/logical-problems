fcc586316355aa95c1cfdda1814718ef9ee5bf5f
gowtham <gowthamm.5127@gmail.com>
2026-03-08 17:55:39 +0530

    Initiate Statisfiability of equality equations

--- leet_code/statisfiability_of_equality_equations.js
+++ leet_code/statisfiability_of_equality_equations.js
@@ -6,12 +6,15 @@ var equationsPossible = function(equations) {
     
 };
 
+
+
 // Example 1:
 
 // Input: equations = ["a==b","b!=a"]
 // Output: false
 // Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
 // There is no way to assign the variables to satisfy both equations.
+
 // Example 2:
 
 // Input: equations = ["b==a","a==b"]
