956fc8e3159adaf43463a8c47b888c16ca6781f3
gowtham <gowthamm.5127@gmail.com>
2026-03-14 23:32:38 +0530

    Added apply operations to make all array elements equal to zero

---
+++ leet_code/apply_operations_to_make_all_array_elements_equal_to_zero.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkArray = function(nums, k) {
+    for (let i=0; i<nums.length; i++) {
+        if(nums[i] < 0) return false;
+        let val = nums[i];
+        if(val > 0) {
+            if(i+k > nums.length) return false;
+            for (let j=i; j<i+k; j++) {
+                nums[j] -= val
+            }
+        }
+    }
+    return true;
+};
+
+let nums = [2,2,3,1,1,0], k = 3
+console.log(checkArray(nums, k))
+
+// Example 1:
+
+// Input: nums = [2,2,3,1,1,0], k = 3
+// Output: true
+// Explanation: We can do the following operations:
+// - Choose the subarray [2,2,3]. The resulting array will be nums = [1,1,2,1,1,0].
+// - Choose the subarray [2,1,1]. The resulting array will be nums = [1,1,1,0,0,0].
+// - Choose the subarray [1,1,1]. The resulting array will be nums = [0,0,0,0,0,0].
+
+
+// Example 2:
+
+// Input: nums = [1,3,1,1], k = 2
+// Output: false
+// Explanation: It is not possible to make all the array elements equal to 0.
