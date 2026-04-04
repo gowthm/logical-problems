5230461060d360bde7ca983f46c5cedec5a5a24e
gowtham <gowthamm.5127@gmail.com>
2026-04-04 10:55:45 +0530

    Added first completely painted row or column

--- leet_code/first_completely_painted_row_or_column.py
+++ leet_code/first_completely_painted_row_or_column.py
@@ -18,4 +18,27 @@ class Solution(object):
 
 sol = Solution()
 sol.firstCompleteIndex([1,3,4,2], [[1,4],[2,3]])
+
+# example 1:
+
+# Input: arr = [1,3,4,2], mat = [[1,4],[2,3]]
+# Output: 2
+# Explanation: The first 3 numbers in arr are 1, 3, and 4. 
+# - 1 and 4 are in the first row. 
+# - 2 and 3 are in the second row. 
+# - 1 and 2 are in the first column. 
+# - 3 and 4 are in the second column. 
+# At index 2, the first column is completely painted.
+
+# example 2:
+
+# Input: arr = [2,5,1,6,4,3], mat = [[1,2,3],[4,5,6]]
+# Output: 5
+# Explanation: The first 6 numbers in arr are 2, 5, 1, 6, 4, and 3. 
+# - 1, 2, and 3 are in the first row. 
+# - 4, 5, and 6 are in the second row. 
+# - 1 and 4 are in the first column. 
+# - 2 and 5 are in the second column. 
+# - 3 and 6 are in the third column. 
+# At index 5, the first row and the third column are completely painted.
         
