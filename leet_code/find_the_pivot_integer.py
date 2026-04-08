3f29174e3e4a4f4f22df483ea430637163f84431
gowtham <gowthamm.5127@gmail.com>
2026-04-08 11:06:36 +0530

    Added find the pivot integer

--- leet_code/find_the_pivot_integer.py
+++ leet_code/find_the_pivot_integer.py
@@ -21,17 +21,16 @@ print(sol.pivotInteger(8))
 
 """
 Example 1:
-
 Input: n = 8
 Output: 6
 Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
-Example 2:
 
+Example 2:
 Input: n = 1
 Output: 1
 Explanation: 1 is the pivot integer since: 1 = 1.
-Example 3:
 
+Example 3:
 Input: n = 4
 Output: -1
 Explanation: It can be proved that no such integer exist.
