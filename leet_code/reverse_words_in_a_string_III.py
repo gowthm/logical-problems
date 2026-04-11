2090e2d73ccbddcd7083543c71c7ccc1f9f3b350
gowtham <gowthamm.5127@gmail.com>
2026-04-11 23:59:58 +0530

    Added reverse words in a string III python

--- leet_code/reverse_words_in_a_string_III.py
+++ leet_code/reverse_words_in_a_string_III.py
@@ -10,4 +10,18 @@ class Solution(object):
 
 sol = Solution()
 print(sol.reverseWords("Let's take LeetCode contest"))
-        
+
+
+"""
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Example 2:
+
+Input: s = "Mr Ding"
+
+Output: "rM gniD"
+"""
