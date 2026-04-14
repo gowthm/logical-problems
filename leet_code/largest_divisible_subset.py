35baf590bd3d53e66aa5d43d6d794f9b2857c471
gowtham <gowthamm.5127@gmail.com>
2026-04-14 15:13:06 +0530

    Added largest divisible subset

---
+++ leet_code/largest_divisible_subset.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+
+sol = Solution()
+print(sol.largestDivisibleSubset([1,2,4,8]))
+
+"""
+Example 1:
+
+Input: nums = [1,2,3]
+
+Output: [1,2]
+
+Explanation: [1,2] is the largest divisible subset with length 2. Another possible answer is [1,3]
+
+Example 2:
+
+Input: nums = [1,2,4,8]
+
+Output: [1,2,4,8]
+
+Explanation: [1,2,4,8] is the largest divisible subset with length 4.
+
+"""
