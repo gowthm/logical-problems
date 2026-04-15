45a1307446894abbd5a468f2ce1ecd8e67d9e219
gowtham <gowthamm.5127@gmail.com>
2026-04-15 10:56:31 +0530

    Added larget divisible subset solution

--- leet_code/largest_divisible_subset.py
+++ leet_code/largest_divisible_subset.py
@@ -1,13 +1,30 @@
 class Solution(object):
     def largestDivisibleSubset(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        
+        if not nums:
+            return []
+        nums.sort()
+        n = len(nums)
+        dp = [1] * n
+        prev = [-1] * n
+        main_index = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j]  == 0:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j]+1
+                        prev[i] = j
+            if dp[i] > dp[main_index]:
+                main_index = i
+        result = []
+        while main_index != -1:
+            result.append(nums[main_index])
+            main_index = prev[main_index]
+        return result[::-1]
+                    
 
 sol = Solution()
-print(sol.largestDivisibleSubset([1,2,4,8]))
+print(sol.largestDivisibleSubset([1,2,3]))
+
 
 """
 Example 1:
