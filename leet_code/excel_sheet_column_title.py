4f4942d5fe068c203225c1887b4ba040d68f6ded
gowtham <gowthamm.5127@gmail.com>
2026-04-17 11:35:20 +0530

    Added excel sheet column title

---
+++ leet_code/excel_sheet_column_title.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        res = ''
+        while columnNumber > 0:
+           columnNumber -= 1
+           char = chr(columnNumber % 26 + ord('A'))
+           res = char + res
+           columnNumber //= 26
+        return  res
+    
+sol = Solution()
+print(sol.convertToTitle(28))
+
