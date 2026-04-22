5682973fe031a0a9142a788a16803df1f74f6098
gowtham <gowthamm.5127@gmail.com>
2026-04-22 15:16:51 +0530

    Added check if all 1s are least length k places away

---
+++ leet_code/check_if_all_1's_are_at_least_length_k_places_away.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def kLengthApart(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
+
+
+'''
+Example 1
+
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+
+Example 2
+
+Input: nums = [1,0,0,1,0,1], k = 2
+Output: false
+Explanation: The second 1 and third 1 are only one apart from each other.
+
+'''
