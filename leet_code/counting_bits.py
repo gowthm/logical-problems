1e0571a009aff2105550f771b02fd0c1a3f73f10
gowtham <gowthamm.5127@gmail.com>
2026-04-25 10:46:21 +0530

    Added counting bits

---
+++ leet_code/counting_bits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countBits(self, n):
+        finalArr = []
+        for i in range(0, n+1):
+            num = bin(i)[2:]
+            total = 0
+            for digits in num:
+                total += int(digits)
+            finalArr.append(total)
+        return finalArr
+            
+          
+
+sol = Solution()
+print(sol.countBits(5))
