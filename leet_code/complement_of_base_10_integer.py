627f681ea0924edbe5f93596da82bece44c76160
gowtham <gowthamm.5127@gmail.com>
2026-05-04 13:53:21 +0530

    added complement of base 10 integer

--- leet_code/complement_of_base_10_integer.py
+++ leet_code/complement_of_base_10_integer.py
@@ -15,3 +15,36 @@ class Solution(object):
 
 sol = Solution()
 print(sol.bitwiseComplement(5))
+
+'''
+Example 1:
+
+Input: nums = [2,2,3,1], k = 4
+
+Output: 2
+
+Explanation:
+
+The subarray [2, 3] has distinct elements {2, 3} whose sum is 2 + 3 = 5, which is ​​​​​​​at least k = 4. Thus, the answer is 2.
+
+Example 2:
+
+Input: nums = [3,2,3,4], k = 5
+
+Output: 2
+
+Explanation:
+
+The subarray [3, 2] has distinct elements {3, 2} whose sum is 3 + 2 = 5, which is ​​​​​​​at least k = 5. Thus, the answer is 2.
+
+Example 3:
+
+Input: nums = [5,5,4], k = 5
+
+Output: 1
+
+Explanation:
+
+The subarray [5] has distinct elements {5} whose sum is 5, which is at least k = 5. Thus, the answer is 1.
+
+'''
