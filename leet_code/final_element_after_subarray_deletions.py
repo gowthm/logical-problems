85942a4ba092903382f5e5afa116acd5b65130c5
gowtham <gowthamm.5127@gmail.com>
2026-05-10 22:20:32 +0530

    Added final element after subarray deletions

--- leet_code/final_element_after_subarray_deletions.py
+++ leet_code/final_element_after_subarray_deletions.py
@@ -4,6 +4,33 @@ class Solution(object):
         if n == 1:
             return nums[0]
         return max(nums[0], nums[-1])
+
+'''
+
+Example 1:
+
+Input: nums = [1,5,2]
+
+Output: 2
+
+Explanation:
+
+One valid optimal strategy:
+
+Alice removes [1], array becomes [5, 2].
+Bob removes [5], array becomes [2]​​​​​​​. Thus, the answer is 2.
+
+Example 2:
+
+Input: nums = [3,7]
+
+Output: 7
+
+Explanation:
+
+Alice removes [3], leaving the array [7]. Since Bob cannot play a turn now, the answer is 7.
+
+'''
             
     
 sol = Solution()
